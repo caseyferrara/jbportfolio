8e0951437fbc906f4222c84b309bf8d203f5b64c
Casey Ferrara <casey.ferrara@outlook.com>
2022-12-20 19:32:32 -0600

    CSS updates for devices

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -14,27 +14,29 @@ function Contact() {
 
     return (
       <div style={classes.contactContainer}>
+
+        <div className="svgContainerHeader">
+          <svg className="svgHeader" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shapeFill"></path>
+          </svg>
+        </div>
         <Grid
             container 
             direction="row" 
             align="center"
-            rowGap={8}
+            rowGap={4}
         >
-          <Grid item xs={12} sm={12}>
-            <h1>Contact Jillian Brown</h1>
+          <Grid className='contact-index' item xs={12} sm={12}>
+            <h1 className='contact-header'>Contact Jillian Brown</h1>
           </Grid>
-          <Grid item xs={12} sm={12}>
+          <Grid className='contact-index' item xs={12} sm={12}>
             <Box
               component="form"
               noValidate
               autoComplete="off"
+              className='contact-form'
               sx={{
-                width: 650,
-                height: 350,
                 backgroundColor: '#E8EAE5',
-                border: 6,
-                borderRadius: 5,
-                borderColor: '#303030'
               }}
             >
                 <TextField
